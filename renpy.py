15bdfb6dce57d77bed2be8b8eb23e71c087ee11a
Phantop <phantop@tuta.io>
2024-11-03 02:13:57 -0500

    write basic preprocessor

--- renpy.py
+++ renpy.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-from sys import argv
-from ripgrepy import Ripgrepy
-from re import compile as re
-from pprint import pp as pprint
+import sys
+from pprint import pp
+from ripgrepy import Ripgrepy as rg
+pprint = pp
+
+
+def _(a: str):
+    '''Ignore gettext translations'''
+    return a
 
 
 class Character:
@@ -14,47 +19,49 @@ class Character:
         print(f"{self.name}: {txt}")
 
 
+class Line:
+    def __init__(self, line: str):
+        self.indent = len(line) - len(line.lstrip())
+        self.keyword = line.split()[0]
+        self.arg_arr = line.split()[1:]
+        self.arg_str = ' '.join(self.arg_arr)
+
+
 class Game:
-    def __init__(self, dir: str):
-        self.dir = dir
-        self.characters: dict[str, Character] = {}
+    def __init__(self, loc: str):
+        self.dir = loc
+        self.__preprocess()
 
-        self.build_labels()
-        self.build_init()
-
-    def rg(self, x: str) -> dict:
-        return Ripgrepy(x, self.dir).glob('*.rpy').json().run().as_dict
-
-    def search(self, regex: str, keep: str = None):
-        matches = self.rg(regex)
-        if keep:
-            output = {}
-        else:
-            output = []
-        for i in matches:
-            if i['type'] == 'match':
-                match = i['data']['lines']['text']
-                path = i['data']['path']['text']
-                line = i['data']['line_number']
-                loc = {'file': path, 'line': line}
-                if keep:
-                    name = re(keep).search(match).group(1)
-                    output[name] = loc
-                else:
-                    output.append(loc)
+    def __search(self, regex: str, keep: int = None):
+        matches = rg(regex, self.dir).glob('*.rpy').json().run().as_dict
+        matches = [m['data'] for m in matches if m['type'] == 'match']
+        output = {} if keep else []
+        for m in matches:
+            file = m['path']['text']
+            line = m['line_number']
+            text = m['submatches'][0]['match']['text']
+            loc = (file, line)
+            if keep:
+                name = text.split()[keep]
+                output[name] = loc
+            else:
+                output.append(loc)
+        pprint(output)
         return output
 
-    def build_labels(self):
-        self.labels = self.search('^ *label .?*:', '^ *label (.*?):')
-        pprint(self.labels)
+    def __preprocess(self):
+        self.labels = self.__search('^ *label [^:]*', 1)
+        self.init = self.__search('^init:')
+        if self.init[0]:
+            self.file = self.init[0][0]
+            self.line = self.init[0][1]
+        else:
+            self.file = self.labels['start'][0]
+            self.line = self.labels['start'][1]
 
-    def build_init(self):
-        self.init = self.search('^init:')
-        pprint(self.init)
+        self.characters: dict[str, Character] = {}
 
 
 if __name__ == '__main__':
-    if len(argv) != 2:
-        exit()
-
-    game = Game(argv[1])
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    game = Game(path)
